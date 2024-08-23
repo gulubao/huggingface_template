f99491eca2eee641bf148f9ee20cf5904521c7c4
gulubao <1047503667@qq.com>
2024-08-23 12:14:03 -0400

    更新训练入口

--- tools/train_net.py
+++ tools/train_net.py
@@ -31,19 +31,5 @@ if __name__ == '__main__':
             debugpy.wait_for_client()
         except Exception as e:
             pass
-    
-    seed = args.seed
-    rank = 0
-    torch.manual_seed(seed + rank)
-    np.random.seed(seed + rank)
-    random.seed(seed + rank)
-
-    if torch.cuda.is_available():
-        # This enables tf32 on Ampere GPUs which is only 8% slower than
-        # float16 and almost as accurate as float32
-        # This was a default in pytorch until 1.12
-        torch.backends.cuda.matmul.allow_tf32 = True
-        torch.backends.cudnn.benchmark = True
-        torch.backends.cudnn.deterministic = False
 
     main(args)
