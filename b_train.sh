69cc9b37cd1a094e78eba0cab0e0eea2ff8f58a0
gulubao <1047503667@qq.com>
2024-08-30 14:37:14 -0400

    # -- remove cache --#

--- b_train.sh
+++ b_train.sh
@@ -6,8 +6,7 @@ free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
 # -- remove cache --#
 BASE_DIR="~/code/research/xxx"
 find $BASE_DIR -type d -name "__pycache__" -not -path "*/reference/*" -exec rm -r {} + # 查找并删除所有__pycache__目录，但排除reference文件夹及其子文件夹
-echo "除reference文件夹外, $BASE_DIR下的所有__pycache__目录已被删除。"
-
+echo "除reference文件夹外, $BASE_DIR 下的所有 __pycache__ 目录已被删除。"
 # -- tmux setup --#
 tmux new -s task_name
 tmux attach -t task_name
