512477076c64f8a433506572c6cbcefee454007e
gulubao <1047503667@qq.com>
2024-08-30 10:05:01 -0400

    改进实验脚本和预处理流程
    - 添加环境设置和缓存清理步骤
    - 引入 tmux 会话管理（仅在 b_train.sh）
    - 增加内存清理和调试选项
    - 更新实验参数设置
    - 统一文件结构和注释风格

--- a_preprocess.sh
+++ a_preprocess.sh
@@ -5,8 +5,9 @@ free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
 
 # -- remove cache --#
 BASE_DIR="~/code/research/xxx"
-find $BASE_DIR -type d -name "__pycache__" -exec rm -r {} + # Find and remove all __pycache__ directories
-echo "All __pycache__ directories under $BASE_DIR have been removed."
+find $BASE_DIR -type d -name "__pycache__" -not -path "*/reference/*" -exec rm -r {} + # 查找并删除所有__pycache__目录，但排除reference文件夹及其子文件夹
+echo "除reference文件夹外, $BASE_DIR下的所有__pycache__目录已被删除。"
+
 
 # -- debug --#
 free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
