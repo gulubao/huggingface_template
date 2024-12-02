fab98cd3d5dd0da0b974dfae416ca19bfd174024
gulubao <1047503667@qq.com>
2024-12-02 15:38:04 -0500

    Fix BASE_DIR path in a_preprocess.sh to use $HOME variable for better portability; ensure consistent environment setup for cache removal.

--- a_preprocess.sh
+++ a_preprocess.sh
@@ -10,7 +10,7 @@ free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
 # -- remove cache all--#
 # ---------------------------------------------------------------- #
 
-BASE_DIR="~/code/research/xxx"
+BASE_DIR="$HOME/code/research/xxx"
 find $BASE_DIR -type d -name "__pycache__" -exec rm -r {} + # 查找并删除所有__pycache__目录
 echo "所有 $BASE_DIR 下的 __pycache__ 目录已被删除。"
 
